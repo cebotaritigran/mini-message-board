d2f5100646511ec17d77ac00286a92f4ccc20658
tigrancebotari <tigrancebotari@gmail.com>
2026-06-20 03:28:39 -0400

    add view engine to app

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const ejs = require('ejs');
 
 //routers
 const homeRouter = require('./routes/homeRouter');
 const newRouter = require('./routes/newRouter');
 const messageRouter = require("./routes/messageRouter");
+
+app.set('view engine', 'ejs');
 
 
 // styles
