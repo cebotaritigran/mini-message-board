7fb7b76ad5de6d5ecc0eb0e276191d957b23aa31
tigrancebotari <tigrancebotari@gmail.com>
2026-06-20 04:14:59 -0400

    change port in app.js

--- app.js
+++ app.js
@@ -1,47 +1,35 @@
-// // app.js
-// const express = require("express");
-// const app = express();
-// const path = require("node:path");
-
-
-// //routers
-// const homeRouter = require('./routes/homeRouter');
-// const newRouter = require('./routes/newRouter');
-// const messageRouter = require("./routes/messageRouter");
-
-
-
-// // styles
-// const assetsPath = path.join(__dirname, "public");
-// app.use(express.static(assetsPath));
-
-// // client side set up for ejs
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "ejs");
-
-
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use('/', homeRouter);
-// app.use('/new', newRouter)
-// app.use('/message', messageRouter)
-
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, "0.0.0.0", () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
-
+// app.js
 const express = require("express");
 const app = express();
+const path = require("node:path");
+
+
+//routers
+const homeRouter = require('./routes/homeRouter');
+const newRouter = require('./routes/newRouter');
+const messageRouter = require("./routes/messageRouter");
+
+
+
+// styles
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+// client side set up for ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', homeRouter);
+app.use('/new', newRouter)
+app.use('/message', messageRouter)
+
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("WORKING ON RAILWAY");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on", PORT);
 });
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT);
-});
