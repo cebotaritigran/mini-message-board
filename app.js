c4ab7c6168558e782712c07926ad95d81db4ff38
tigrancebotari <tigrancebotari@gmail.com>
2026-06-20 03:37:52 -0400

    Fix Railway start command

--- app.js
+++ app.js
@@ -2,14 +2,13 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
-const ejs = require('ejs');
+
 
 //routers
 const homeRouter = require('./routes/homeRouter');
 const newRouter = require('./routes/newRouter');
 const messageRouter = require("./routes/messageRouter");
 
-app.set('view engine', 'ejs');
 
 
 // styles
